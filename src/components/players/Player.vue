9194e6bc7239463e7ddc1cf793244d3baa8f851d
Mark Allen <markdallen@me.com>
2021-01-25 21:33:19 +0000

    add players and player components

---
+++ src/components/players/Player.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <input
+      type="text"
+      v-model="player.name"
+    >
+    <button @click="deletePlayer(player)">Delete</button>
+  </div>
+</template>
+
+<script>
+  import { mapActions } from 'vuex';
+
+  export default {
+    methods: {
+      ...mapActions([
+        'deletePlayer',
+      ]),
+    },
+    props: ['player'],
+  };
+</script>
---
+++ src/components/players/Players.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <player
+      v-for="player in players"
+      :key="player.id"
+      :player="player"
+    />
+    <div v-if="newPlayerInputVisible">
+      <input v-model="newPlayerName" type="text">
+      <button @click="addNewPlayer" :disabled="playerExists">Add</button>
+    </div>
+    <button @click="toggleNewPlayerInput">
+      {{ newPlayerInputVisible ? 'Cancel' : 'New Player' }}
+    </button>
+  </div>
+</template>
+
+<script>
+  import Player from './Player.vue';
+  import { mapGetters } from 'vuex';
+
+  export default {
+    data() {
+      return {
+        newPlayerInputVisible: false,
+        newPlayerName: '',
+      };
+    },
+    computed: {
+      ...mapGetters([
+        'players',
+      ]),
+      playerExists() {
+        return this.players.some(player => player.name === this.newPlayerName);
+      }
+    },
+    created() {
+      if (this.players.length === 0) {
+        this.toggleNewPlayerInput();
+      }
+    },
+    methods: {
+      toggleNewPlayerInput() {
+        this.newPlayerInputVisible = !this.newPlayerInputVisible;
+      },
+      addNewPlayer() {
+        this.$store.dispatch('addPlayer', {
+          id: require('uniqid').process(),
+          name: this.newPlayerName,
+          multiplier: false,
+        });
+        this.toggleNewPlayerInput()
+        this.newPlayerName = '';
+      },
+    },
+    components: {
+      Player,
+    },
+  };
+</script>
