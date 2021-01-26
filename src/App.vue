ec1152153c3db1f8c49913b8653a557d388bb5a0
Mark Allen <markdallen@me.com>
2021-01-26 21:05:44 +0000

    add logic to switch between active components, edit players and start/end game

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,68 @@
 <template>
   <div>
     <h1>Everybody Dies</h1>
-    <component :is="activeComponent"/>
+    <h2 v-if="status">Current player: {{ currentPlayer.name }}</h2>
+    <div v-if="!status && component === 'splash'">
+      <button @click="selectComponent('players')">Players</button>
+      <button @click="checkPlayersAndStartGame">Start Game</button>
+    </div>
+    <button v-else-if="status" @click="closeGameAndGoHome">End Game</button>
+    <component
+      :is="component"
+      @players-closed="selectComponent('splash')"
+    />
   </div>
 </template>
 
 <script>
   import Players from './components/players/Players.vue';
+  import Splash from './components/Splash.vue';
+  import CardSelection from './components/CardSelection.vue';
+  import { mapActions, mapGetters } from 'vuex';
 
   export default {
+    data() {
+      return {
+        component: 'splash',
+      };
+    },
     computed: {
-      activeComponent() {
-        return 'players';
+      ...mapGetters([
+        'players',
+        'currentPlayer',
+        'status',
+      ]),
+    },
+    methods: {
+      ...mapActions([
+        'startGame',
+        'endGame',
+      ]),
+      created() {
+        console.log(this.currentPlayer);
+      },
+      checkPlayersAndStartGame() {
+        if (this.players.length > 0) {
+          this.startGame();
+          this.selectComponent('card-selection');
+        } else {
+          // @TODO ADD ERROR HANDLING
+          alert('You need at least 1 player');
+          this.selectComponent('players');
+        }
+      },
+      closeGameAndGoHome() {
+        this.endGame();
+        this.selectComponent('splash');
+      },
+      selectComponent(component) {
+        this.component = component;
       },
     },
     components: {
+      Splash,
       Players,
+      CardSelection,
     },
   };
 </script>
