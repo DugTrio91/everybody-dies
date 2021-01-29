b0256a745921465be3b255297d568e0e01e4e8f2
Mark Allen <markdallen@me.com>
2021-01-29 09:35:27 +0000

    add multiplier component

---
+++ src/components/cards/Multiplier.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>The Multiplier</h1>
+    <p>Add rules for the Multiplier here</p>
+    <button @click="dismiss">Dismiss</button>
+  </div>
+</template>
+<script>
+  import { mapActions, mapGetters } from 'vuex';
+
+  export default {
+    computed: {
+      ...mapGetters([
+        'currentPlayer',
+        'players'
+      ]),
+    },
+    created() {
+      this.activateMultiplier(this.players.length);
+      this.setMultiplierToPlayer(this.currentPlayer);
+    },
+    methods: {
+      ...mapActions([
+        'activateMultiplier',
+        'setMultiplierToPlayer',
+      ]),
+      dismiss() {
+        this.$emit('dismissed');
+      },
+    },
+  };
+</script>
