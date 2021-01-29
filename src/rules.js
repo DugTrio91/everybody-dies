affe2e2199b76afe584ba17e351229333ffa7ec3
Mark Allen <markdallen@me.com>
2021-01-29 09:30:37 +0000

    move rules to own js file

---
+++ src/rules.js
@@ -0,0 +1,98 @@
+export const randomiser = [
+  {
+    instruction: 'Finish your drink immediately, unaffected by multiplier',
+    drinks: 1,
+    multipliable: false,
+  },
+  {
+    instruction: 'Take 2 drinks',
+    drinks: 2,
+    multipliable: true,
+  },
+  {
+    // Extra logic required
+    instruction: 'Donate 3 drinks (no single player can be donated all 3)',
+    drinks: 1,
+    multipliable: false,
+  },
+  {
+    instruction: 'All girls drink',
+    drinks: 1,
+    multipliable: true,
+  },
+  {
+    instruction: 'All boys drink',
+    drinks: 1,
+    multipliable: true,
+  },
+  {
+    instruction: 'Ethnic minorities drink',
+    drinks: 1,
+    multipliable: true,
+  },
+  {
+    instruction: 'Blondes drink',
+    drinks: 1,
+    multipliable: true,
+  },
+  {
+    instruction: 'Brunettes drink',
+    drinks: 1,
+    multipliable: true,
+  },
+  {
+    instruction: 'Redheads drink',
+    drinks: 1,
+    multipliable: true,
+  },
+  {
+    instruction: 'Blue eyes drink',
+    drinks: 1,
+    multipliable: true,
+  },
+  {
+    instruction: 'Brown eyes drink',
+    drinks: 1,
+    multipliable: true,
+  },
+  {
+    instruction: 'Eye colours other than blue or brown drink',
+    drinks: 1,
+    multipliable: true,
+  },
+  {
+    instruction: 'Left handed players drink',
+    drinks: 1,
+    multipliable: true,
+  },
+  {
+    instruction: 'Right handed players drink',
+    drinks: 1,
+    multipliable: true,
+  },
+  {
+    instruction: 'Over 30 year olds drink',
+    drinks: 1,
+    multipliable: true,
+  },
+  {
+    instruction: 'Under 30 year olds drink',
+    drinks: 1,
+    multipliable: true,
+  },
+  {
+    instruction: 'Go to the toilet immediately, do not take your drink, try for a tactical',
+    drinks: 1,
+    multipliable: false,
+  },
+  {
+    instruction: 'Everyone takes 1 drink',
+    drinks: 1,
+    multipliable: true,
+  },
+  {
+    instruction: 'Everyone but the roller takes 3 drinks',
+    drinks: 3,
+    multipliable: true,
+  },
+];
