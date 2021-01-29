45ea60ecbfe0af7878b4929fa8139fbafa6e494e
Mark Allen <markdallen@me.com>
2021-01-29 09:33:40 +0000

    update card selection to shuffle cards and move between cards and players

--- src/components/CardSelection.vue
+++ src/components/CardSelection.vue
@@ -4,35 +4,70 @@
       class="l-card-selection__card card-selection__card"
       v-for="(card, index) in shuffledCards"
       :key="index"
-      @click="endTurn"
+      @click="revealCard(card)"
+      :disabled="selectedCard"
     >
       {{ card }}
     </button>
+    <component :is="selectedCard" @dismissed="endTurn"/>
   </div>
 </template>
 
 <script>
+  import Multiplier from './cards/Multiplier.vue';
+  import Randomiser from './cards/Randomiser.vue';
+
   import { mapActions, mapGetters } from 'vuex';
 
   export default {
+    data() {
+      return {
+        selectedCard: null,
+        filteredCards: [],
+      };
+    },
     computed: {
       ...mapGetters([
         'cards',
+        'multiplierActive',
       ]),
       shuffledCards() {
-        return this.shuffle(this.cards);
+        return this.shuffle();
       },
+    },
+    created() {
+      this.removeMultiplierFromDeckIfActive();
     },
     methods: {
       ...mapActions([
         'nextPlayer',
       ]),
-      shuffle(deck) {
-        return deck.sort(() => Math.random() - 0.5);
+      shuffle() {
+        return this.filteredCards.sort(() => Math.random() - 0.5);
+      },
+      removeMultiplierFromDeckIfActive() {
+        if (!this.multiplierActive) {
+          this.filteredCards = this.cards;
+        } else {
+          this.filteredCards = this.cards.filter(card => card !== 'multiplier');
+        }
+      },
+      revealCard(card) {
+        this.selectedCard = card;
       },
       endTurn() {
         this.nextPlayer();
+        this.removeMultiplierFromDeckIfActive();
+        this.shuffle();
+        this.showAllCards();
       },
+      showAllCards() {
+        this.selectedCard = null;
+      },
+    },
+    components: {
+      Multiplier,
+      Randomiser,
     },
   };
 </script>
