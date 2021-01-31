4b5ccbb4ed46bcb3302fcc88d39c826bafe3683c
Mark Allen <markdallen@me.com>
2021-01-31 19:48:01 +0000

    expand dice method to include multiplication and add separate multiple and generate methods

--- src/mixins/dice.js
+++ src/mixins/dice.js
@@ -1,7 +1,19 @@
 export const dice = {
   methods: {
-    roll(max) {
-      return Math.floor(Math.random() * max)
-    }
-  }
-}
+    roll(max, multiply = false) {
+      const roll = this.generate(max);
+
+      if (!multiply) {
+        return roll;
+      } else {
+        return this.multiply(roll);
+      }
+    },
+    multiply(roll) {
+      return roll * this.generate(4);
+    },
+    generate(max) {
+      return Math.floor(Math.random() * max) + 1;
+    },
+  },
+};
