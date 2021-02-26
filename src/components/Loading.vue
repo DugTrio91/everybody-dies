a1ef8feecbbefa9acdb5af770d5efddeca5e6c1d
Mark Allen <markdallen@me.com>
2021-02-26 08:46:19 +0000

    create simple loading component

---
+++ src/components/Loading.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="loading">
+    <img
+      class="loading__image"
+      src="../assets/loading.gif"
+      alt=""
+    >
+    <p class="loading__text">
+      {{ player }}, please wait while your fate is decided...</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      player: {
+        type: String,
+        required: true,
+      },
+    },
+  };
+</script>
+
+<style lang="scss">
+  .loading__image {
+    height: 5rem;
+    margin-bottom: 1rem;
+  }
+</style>
