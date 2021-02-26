869ca90db01645c275b85bed26e0ba1807a861d1
Mark Allen <markdallen@me.com>
2021-02-26 08:49:03 +0000

    add styling for player and players components

--- src/components/players/Player.vue
+++ src/components/players/Player.vue
@@ -1,10 +1,15 @@
 <template>
-  <div>
+  <div class="l-player player">
     <input
+      class="l-player__input player__input"
       type="text"
       v-model="player.name"
+      disabled
     >
-    <button @click="deletePlayer(player)">Delete</button>
+    <button
+      class="l-player__delete player__delete btn"
+      @click="deletePlayer(player)"
+    />
   </div>
 </template>
 
@@ -20,3 +25,36 @@
     props: ['player'],
   };
 </script>
+
+<style lang="scss">
+  .l-player {
+    display: flex;
+    align-items: center;
+
+    &__input {
+      margin: 1rem;
+      padding: 0.5rem;
+    }
+
+    &__delete {
+      height: 5rem;
+      width: 5rem;
+    }
+  }
+
+  .player__input {
+    background-color: #322821;
+    color: #fff;
+    outline: 0.5rem groove #7c6856;
+  }
+
+  .player__delete {
+    background: url('~@/assets/buttons/button-action-delete.png') center no-repeat;
+    background-size: contain;
+
+    &:hover {
+      background: url('~@/assets/buttons/button-action-delete-hover.png') center no-repeat;
+      background-size: contain;
+    }
+  }
+</style>
--- src/components/players/Players.vue
+++ src/components/players/Players.vue
@@ -1,18 +1,35 @@
 <template>
-  <div>
+  <div class="l-players players">
+    <h1>Edit Players</h1>
     <player
       v-for="player in players"
       :key="player.id"
       :player="player"
     />
-    <div v-if="newPlayerInputVisible">
-      <input v-model="newPlayerName" type="text">
-      <button @click="addNewPlayer" :disabled="playerExists">Add</button>
+    <hr
+      class="l-players__divide players__divide"
+      v-if="newPlayerInputVisible && players.length > 0"
+    >
+    <div class="l-players__new players__new" v-if="newPlayerInputVisible">
+      <input
+        class="l-players__new-input players__new-input"
+        v-model="newPlayerName"
+        type="text"
+      >
+      <button
+        class="l-players__new-button players__new-button btn"
+        @click="addNewPlayer"
+        :disabled="playerExists"
+      />
     </div>
-    <button @click="toggleNewPlayerInput">
+    <hr class="l-players__divide players__divide">
+    <button class="l-players__action players__action" @click="toggleNewPlayerInput">
       {{ newPlayerInputVisible ? 'Cancel' : 'New Player' }}
     </button>
-    <button @click="closePlayers">Close</button>
+    <button
+      class="btn btn--fixed-right btn-end"
+      @click="closePlayers"
+    />
   </div>
 </template>
 
@@ -33,7 +50,7 @@
       ]),
       playerExists() {
         return this.players.some(player => player.name === this.newPlayerName);
-      }
+      },
     },
     created() {
       if (this.players.length === 0) {
@@ -46,19 +63,88 @@
       },
       addNewPlayer() {
         this.$store.dispatch('addPlayer', {
-          id: require('uniqid').process(),
+          id: require('uniqid')
+            .process(),
           name: this.newPlayerName,
           multiplier: false,
         });
-        this.toggleNewPlayerInput()
+        this.toggleNewPlayerInput();
         this.newPlayerName = '';
       },
       closePlayers() {
         this.$emit('players-closed');
-      }
+      },
     },
     components: {
       Player,
     },
   };
 </script>
+
+<style lang="scss">
+  @import "../../scss/app";
+
+  .players {
+    &__divide {
+      border-color: #b38242;
+    }
+
+    &__new-input {
+      background-color: #3b3219;
+      color: #c5924a;
+      outline: 0.5rem groove #b38242;
+    }
+
+    &__new-button {
+      background: url('~@/assets/buttons/button-action-add.png') center no-repeat;
+      background-size: contain;
+
+      &:hover {
+        background: url('~@/assets/buttons/button-action-add-hover.png') center no-repeat;
+        background-size: contain;
+      }
+    }
+
+    &__action {
+      height: 6rem;
+      width: 13rem;
+      background: url('~@/assets/buttons/button-dismiss.png') center no-repeat;
+      background-size: contain;
+      outline: none;
+      border: 0;
+      font-family: $ed-body-font;
+      font-size: 1.5rem;
+      color: #fff;
+
+      &:hover {
+        background: url('~@/assets/buttons/button-dismiss-hover.png') center no-repeat;
+        background-size: contain;
+      }
+    }
+  }
+
+  .l-players {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    &__divide {
+      width: 100%;
+    }
+
+    &__new {
+      display: flex;
+      align-items: center;
+
+      &-input {
+        margin: 1rem;
+        padding: 0.5rem;
+      }
+
+      &-button {
+        height: 5rem;
+        width: 5rem;
+      }
+    }
+  }
+</style>
