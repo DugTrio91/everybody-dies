b6cb981244a6f17a2b27a33c69cbe1bed17597f9
Mark Allen <markdallen@me.com>
2021-02-26 08:49:20 +0000

    add readable drinks filter and timer mixin

--- src/main.js
+++ src/main.js
@@ -4,6 +4,19 @@ import store from './store/store';
 
 Vue.config.productionTip = false;
 
+Vue.mixin({
+  data: () => ({
+    loadingTimer() {
+      return 4000;
+    },
+  }),
+});
+
+Vue.filter(
+  'readableDrinks',
+  drinks => drinks > 1 ? `${drinks} drinks` : `${drinks} drink`,
+);
+
 new Vue({
   render: (h) => h(App),
   store,
