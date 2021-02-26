ba728dc571b507e9f5d90bfda6c1ec622c11e672
Mark Allen <markdallen@me.com>
2021-02-26 08:47:06 +0000

    separate dismiss button into it's own component

---
+++ src/components/controls/Dismiss.vue
@@ -0,0 +1,46 @@
+<template>
+  <button
+    class="dismiss"
+    :class="{ 'disabled' : loading }"
+    :loading="loading"
+    @click="$emit('click')"
+  >
+    Dismiss
+  </button>
+</template>
+<script>
+  export default {
+    props: {
+      loading: {
+        type: Boolean,
+        required: false,
+        default: false,
+      },
+    },
+  };
+</script>
+<style lang="scss">
+  @import "../../scss/app";
+
+  .dismiss {
+    height: 6rem;
+    width: 13rem;
+    background: url('~@/assets/buttons/button-dismiss.png') center no-repeat;
+    background-size: contain;
+    outline: none;
+    border: 0;
+    font-family: $ed-body-font;
+    font-size: 1.5rem;
+    color: #fff;
+
+    &.disabled {
+      opacity: 0.6;
+      cursor: not-allowed;
+    }
+
+    &:not(.disabled):hover {
+      background: url('~@/assets/buttons/button-dismiss-hover.png') center no-repeat;
+      background-size: contain;
+    }
+  }
+</style>
